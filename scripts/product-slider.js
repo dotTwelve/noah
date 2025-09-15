ad762d4fac73f30f7025bb1158422c4f6ba6de55
Jan Machač <dotTwelve@users.noreply.github.com>
2025-09-15 15:47:12 +0200

    Update product-slider.js

--- scripts/product-slider.js
+++ scripts/product-slider.js
@@ -94,7 +94,7 @@
         createCustomNavigation: function(sliderId) {
             const prevButton = `
                 <a href="#${sliderId}" 
-                   class="carousel-nav carousel-prev btn fg ico-md sh-md ml-n4 NoScroll bg-de swiper-button-prev-custom" 
+                   class="carousel-nav carousel-prev btn fg ico-md sh-md ml-n0 NoScroll bg-de swiper-button-prev-custom" 
                    role="button" 
                    rel="nofollow"
                    aria-label="Předchozí">
@@ -107,7 +107,7 @@
             
             const nextButton = `
                 <a href="#${sliderId}" 
-                   class="carousel-nav carousel-next btn fg ico-md sh-md mr-n4 NoScroll bg-de swiper-button-next-custom" 
+                   class="carousel-nav carousel-next btn fg ico-md sh-md mr-n0 NoScroll bg-de swiper-button-next-custom" 
                    role="button" 
                    rel="nofollow"
                    aria-label="Další">
